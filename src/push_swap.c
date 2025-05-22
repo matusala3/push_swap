e542f9b56795cc0358d0c2add3b1a0deb67067f3
matusala3 <matusalakiflom@gmail.com>
2025-05-22 21:36:52 +0300

    Refactor main function and add quick sort utilities

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,10 +1,14 @@
 #include "../include/parsing_utils.h"
 #include "../include/push_swap.h"
 
+int	*copy_stack_to_array(t_stack stack_a, int size);
+
 int	main(int argc, char **argv)
 {
 	t_stack	stack_a;
-	t_node	*current;
+	//t_node	*current;
+	int *linkedlist_copy;
+	int i = 0;
 
 	stack_a.head = NULL;
 	stack_a.tail = NULL;
@@ -17,29 +21,40 @@ int	main(int argc, char **argv)
 		write(2, "Error\n", 6);
 		return (1);
 	}
-	current = stack_a.head;
-	printf("Stack A: \n head -> ");
-	if (!current)
-	{
-		printf("(empty)");
-	}
-	else
-	{
-		while (current)
-		{
-			printf("[%d]", current->value);
-			if (current->next)
-				printf("and its next is %d ", current->next->value);
-			else
-				printf("has no next ");
-			if (current->prev)
-				printf("and its prev is %d ", current->prev->value);
-			else
-				printf("has no prev ");
-			current = current->next;
-		}
-		printf(" <- tail");
-	}
+	// current = stack_a.head;
+	// printf("Stack A: \n head -> ");
+	// if (!current)
+	// {
+	// 	printf("(empty)");
+	// }
+	// else
+	// {
+	// 	while (current)
+	// 	{
+	// 		printf("[%d]", current->value);
+	// 		if (current->next)
+	// 			printf("and its next is %d ", current->next->value);
+	// 		else
+	// 			printf("has no next ");
+	// 		if (current->prev)
+	// 			printf("and its prev is %d ", current->prev->value);
+	// 		else
+	// 			printf("has no prev ");
+	// 		current = current->next;
+	// 	}
+	// 	printf(" <- tail");
+	// }
+
+
+	// linkedlist_copy = copy_stack_to_array(stack_a, stack_a.size);
+	// while (stack_a.size > i)
+	// {
+	// 	printf("arr[%d] = %d \n", i, linkedlist_copy[i]);
+	// 	i++;
+	// }
+	// free(linkedlist_copy);
+
+
 	free_stack(&stack_a);
 	return (0);
 }
@@ -61,4 +76,37 @@ int	*copy_stack_to_array(t_stack stack_a, int size)
 		i++;
 	}
 	return arr;
+}
+
+void quick_sort(int *arr, int low, int high)
+{
+
+}
+
+int median_of_three(int *arr, int low, int high)
+{
+	int mid;
+	
+	mid = (low + high) / 2;
+	int a = arr[low];
+	int b = arr[mid];
+	int c = arr[high];
+	if((a < b && b < c) ||(c < b && b < a))
+		return(mid);
+	else if((b < a && a < c) || (c < a && a < b))
+		return (low);
+	else
+		return (high);
+}
+
+int partition(int *arr, int low, int high)
+{
+	int pivot_val;
+	int temp;
+	int median;
+
+	pivot_val = arr[median];
+	temp = arr[high];
+	arr[high] = pivot_val;
+	arr[median] = temp;
 }

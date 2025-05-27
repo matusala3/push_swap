cb3e2bc67053be19188cf482d261f6a4bdeebf78
matusala3 <matusalakiflom@gmail.com>
2025-05-27 17:00:06 +0300

    Reorder sorting logic in check_duplicates function for clarity

--- src/check_duplicate.c
+++ src/check_duplicate.c
@@ -83,9 +83,9 @@ bool check_duplicates(t_stack *stack_a)
 		return (false);
 	linkedlist_copy = copy_stack_to_array(*stack_a, stack_a->size);
 	if(size < 15 && size > 2)
-		insertion_sort(linkedlist_copy, size);
-	else if (size < 250 && size > 15)
 		quick_sort(linkedlist_copy, 0, size - 1);
+	else if (size < 250 && size > 15)
+		insertion_sort(linkedlist_copy, size);
 	else
 		heap_sort(linkedlist_copy, size);
 	for(int i = 0; i < size; i++)
